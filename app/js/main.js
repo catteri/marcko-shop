4c1bfca865964b66ffc0703766783f9120322d61
catteri <caterinascob@gmail.com>
2021-01-29 21:01:10 +0100

    add full

---
+++ app/js/main.js
@@ -0,0 +1,116 @@
+$(function () {
+
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade:true
+  });
+    
+
+  $('.select-style, .product-item__filter-num').styler();
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product__item').addClass('product__item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product__item').removeClass('product__item--list');
+  });
+  
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__from').text(data.to);
+        },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__from').text(data.to);
+        },
+
+  });
+
+  
+
+    $('.top__slider-inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpead:2000
+    });
+
+    $('.star').rateYo({
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        rateFill: "#ffc35b",
+        numStars: 5,
+        readOnly:true
+        
+    });
+
+    function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.querySelector('.promo__clock');
+  const daysSpan = clock.querySelector('.promo__days');
+  const hoursSpan = clock.querySelector('.promo__hours');
+  const minutesSpan = clock.querySelector('.promo__minutes');
+  const secondsSpan = clock.querySelector('.promo__seconds');
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+const deadline = $('.promo__clock').attr('data-time');
+initializeClock('promo__clock', deadline);
+    
+/*     const deadline = '2021-05-25';
+initializeClock('promo__clock', deadline); */
+
+});
